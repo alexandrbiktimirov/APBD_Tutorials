bd5d7a5cad0669830b1baf4456ac7d0e6635a050
s30175 <alex.biktimirov38@gmail.com>
2025-05-11 14:41:12 +0200

    Add Tutorial4, Tutorial4Tests, Tutorial6 to the APBD_Tutorials

---
+++ Tutorial6/Database.cs
@@ -0,0 +1,20 @@
+﻿using Tutorial6.Models;
+
+namespace Tutorial6;
+
+public static class Database
+{
+    public static List<Animal> Animals = new List<Animal>()
+    {
+        new Animal() { Id = 1, Name = "Animal 1", Category = "Cat", Weight = 3, FurColor = "Black"},
+        new Animal() { Id = 2, Name = "Animal 2", Category = "Dog", Weight = 10, FurColor = "White" },
+        new Animal() { Id = 3, Name = "Animal 3", Category = "Horse", Weight = 20, FurColor = "Brown" },
+    };
+    
+    public static readonly List<Visit> Visits = new List<Visit>()
+    {
+        new Visit() { Id = 1, Animal = 1, Description = "Description of visit 1", Price = 100},
+        new Visit() { Id = 2, Animal = 2, Description = "Description of visit 2", Price = 200},
+        new Visit() { Id = 3, Animal = 3, Description = "Description of visit 3", Price = 300},
+    };
+}
---
+++ Tutorial6/Models/Visit.cs
@@ -0,0 +1,10 @@
+﻿namespace Tutorial6.Models;
+
+public class Visit
+{
+    public int Id { get; set; }
+    public string Date { get; set; }
+    public int Animal { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+}
